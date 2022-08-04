4ddcce0a222659ad7eb13be2fa9223803fb9f870
Ben_Bait <bmaloy01@eecs.tufts.edu>
2022-08-03 23:04:19 -0400

    added idea to imports in get_laplacian.c

--- get_laplacian.c
+++ get_laplacian.c
@@ -1,5 +1,9 @@
 #include "iostream"
 
+// maybe use matrix as a header file instead of a file that 
+// produces separate matrix binaries
+#include "Matrix.h"
+
 int main(int argc, char *argv[]) {
 
     return 0;
