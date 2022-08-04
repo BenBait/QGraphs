d9dd498b881df3b030e8a07e906a9a91091097aa
Ben_Bait <bmaloy01@eecs.tufts.edu>
2022-08-03 21:44:35 -0400

    testing script

---
+++ post.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script needs one argument
+# argc -> $#   ;    argv[1] = $1
+
+if [ $# -ne 1 ]
+then
+    echo "Pass one argument"
+    exit 0
+fi
+
+# add to git stage
+git add .
+
+# commit the changes
+git commit -m "$1"
+
+# push it to github server
+git push
+
+done
